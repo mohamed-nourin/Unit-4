bebf738815da8711f2bc09335b92d63c940e4b9b
Mohamed Nourin <mohamed0nourin@gmail.com>
2023-09-10 16:08:18 +0300

    working

---
+++ src/main/java/com/devmountain/noteApp/entites/Note.java
@@ -0,0 +1,37 @@
+package com.devmountain.noteApp.entites;
+
+import com.devmountain.noteApp.dtos.NoteDto;
+import com.devmountain.noteApp.entites.User;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "Notes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "text")
+    private String body;
+
+    @ManyToOne
+    @JsonBackReference
+    private User user;
+
+    public Note(NoteDto noteDto){
+        if (noteDto.getBody() != null){
+            this.body = noteDto.getBody();
+        }
+    }
+
+}
--- src/main/java/com/devmountain/noteApp/entities/User.java
+++
@@ -1,8 +0,0 @@
-package com.devmountain.noteApp.entities;
-
-import jakarta.persistence.Entity;
-
-@Entity
-@Table(name = "Users")
-public class User {
-}
